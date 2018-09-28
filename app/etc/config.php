8188e4fc0838a238f3a1cc060d911b4ed25e045f
Nebojsa Novic <nebojsa.novic79@gmail.com>
2018-09-28 15:52:46 +0200

    new_module_declaration

--- app/etc/config.php
+++ app/etc/config.php
@@ -124,6 +124,7 @@ return [
         'Magento_CatalogWidget' => 1,
         'Dotdigitalgroup_Email' => 1,
         'Magento_WishlistAnalytics' => 1,
+        'Mastering_SampleModule' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1
     ]
